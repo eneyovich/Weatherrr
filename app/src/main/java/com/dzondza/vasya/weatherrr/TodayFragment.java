4b21e87e7c6eb0d701bcabf2d6ded0d80bb21f2f
vasya <eneyovich@gmail.com>
2017-09-15 12:05:03 +0300

    commit

--- app/src/main/java/com/dzondza/vasya/weatherrr/TodayFragment.java
+++ app/src/main/java/com/dzondza/vasya/weatherrr/TodayFragment.java
@@ -1,5 +1,7 @@
 package com.dzondza.vasya.weatherrr;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
